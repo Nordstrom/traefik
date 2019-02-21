b1f9b40604476472d558590326cc8b72df9932e0
Will Kamp <will.kamp@nordstrom.com>
2019-02-21 10:42:04 -0800

    scripts and Dockerfile to bake debuggable image

--- nmake.sh
+++ nmake.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo chown -R willard:willard ${DIR}/static
-pushd ${DIR}
-make image
-docker tag containous/traefik gitlab-registry.nordstrom.com/gtm/linkerd-sandbox/traefik:v1.7$1
-docker push gitlab-registry.nordstrom.com/gtm/linkerd-sandbox/traefik:v1.7$1
-popd
 
+tag=gitlab-registry.nordstrom.com/gtm/linkerd-sandbox/traefik:v1.7$2
+
+debug() {
+    pushd ${DIR}
+    docker build -t ${tag} -f Dockerfile.debug .
+    docker push ${tag}
+    popd
+}
+
+release() {
+    pushd ${DIR}
+    make image
+    docker tag containous/traefik ${tag}
+    docker push ${tag}
+    popd
+}
+
+case $1 in
+    "debug")
+    debug $2
+    ;;
+    "release")
+    release $2
+    ;;
+    *)
+    echo "release|debug"
+    exit 1
+    ;;
+esac
